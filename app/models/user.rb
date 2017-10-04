ea6a98ecbf5f8cff88d0e053b7ef6de4bc863670
amitk <amit@joshsoftware.com>
2017-10-04 17:56:35 +0530

    FactoryGirl integrated with Rspec
    Test environment setup successful and user model generated

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User
+  include Mongoid::Document
+
+  field :name,  type: String
+  field :email, type: String
+
+  validates :name, :email, presence: true
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it "registered user must have a name" do
+    user = create :user
+  end
+
+  it "registered user must have a email" do
+  end
+
+  it "registered user must have a password" do
+  end
+
+  it "registered user cannot upload videos" do
+  end
+
+  it "registered user can view free and registered videos" do
+  end
+
+  it "registered user can't view subscribed videos" do
+  end
+
+  it "subscribed user must be a registered user" do
+  end
+
+  it "subscribed user must have a name" do
+  end
+
+  it "subscribed user must have a email" do
+  end
+
+  it "subscribed user must have a password" do
+  end
+
+  it "subscribed user must have payment details" do
+  end
+
+  it "subscribed user can view free, registered and subscribed video" do
+  end
+
+  it "subscribed user can't upload videos" do
+  end
+
+  it "user with role admin can upload videos" do
+  end
+
+  it "user with role admin can delete videos" do
+  end
+
+  it "user with role admin can view free, registered and subscribed video" do
+  end
+end
